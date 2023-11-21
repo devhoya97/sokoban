fd3e7f2cd46b5a63bbb7dafc7e3551e8f14a833f
devhoya97 <diajbh123@gmail.com>
2023-11-21 17:34:06 +0900

    feat: 입력 커맨드가 방향을 나타낼 때 사용할 enum 구현

---
+++ src/main/java/sokoban/Direction.java
@@ -0,0 +1,33 @@
+package sokoban;
+
+public enum Direction {
+    UP('w', "위쪽으로 이동합니다."),
+    DOWN('s', "아래쪽으로 이동합니다."),
+    LEFT('a', "왼쪽으로 이동합니다."),
+    RIGHT('d', "오른쪽으로 이동합니다.");
+
+    private final char symbol;
+    private final String moveMessage;
+
+    Direction(char symbol, String moveMessage) {
+        this.symbol = symbol;
+        this.moveMessage = moveMessage;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public String getMoveMessage() {
+        return moveMessage;
+    }
+
+    public static Direction match(char symbol) {
+        for (Direction direction : Direction.values()) {
+            if (direction.symbol == symbol) {
+                return direction;
+            }
+        }
+        return null;
+    }
+}
