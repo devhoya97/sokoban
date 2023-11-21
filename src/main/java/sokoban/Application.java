6dbe273b6bbe8846838eb4af32af400b12668fc2
devhoya97 <diajbh123@gmail.com>
2023-11-21 19:41:35 +0900

    feat: 게임을 전체적으로 진행하는 기능 구현

--- src/main/java/sokoban/Application.java
+++ src/main/java/sokoban/Application.java
@@ -2,13 +2,28 @@ package sokoban;
 
 import java.util.ArrayList;
 import java.util.List;
+import sokoban.view.InputView;
+import sokoban.view.OutputView;
 
 public class Application {
     private static final String NEW_LINE = "\n";
     private static final String STAGE_DELIMITER = "=====";
+    private static final char QUIT = 'q';
 
     public static void main(String[] args) {
-        List<GameMap> gameMaps = getGameMaps();
+        GameMap gameMap = getGameMaps().get(1);
+        OutputView.printGameStart(gameMap);
+        while (true) {
+            String userInput = InputView.getUserInput();
+            for (char symbol : userInput.toCharArray()) {
+                if (symbol == QUIT) {
+                    OutputView.printGameOver();
+                    return;
+                }
+                boolean canMove = gameMap.move(symbol);
+                OutputView.printResult(gameMap, symbol, canMove);
+            }
+        }
     }
 
     private static List<GameMap> getGameMaps() {
